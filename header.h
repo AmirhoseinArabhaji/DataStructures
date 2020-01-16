8937494b20a38fd9607b42323d296800a9998d87
Amirhosein Arabhaji <a.arab12345@gmail.com>
2020-01-16 13:04:51 +0330

    header.h added

---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef DATA_STRUCTURES_HEADER_H
+#define DATA_STRUCTURES_HEADER_H
+
+#include <iostream>
+#include <algorithm>
+#include "exception.h"
+
+using namespace std;
+
+#endif //DATA_STRUCTURES_HEADER_H
